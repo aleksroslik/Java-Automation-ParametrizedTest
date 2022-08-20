bfd292bc49fa50518528c8be6e1043dddd1bf417
JavaClass <aleksroslik@gmail.com>
2022-08-20 16:18:48 +0200

    task finished

---
+++ src/test/java/DataProvider.java
@@ -0,0 +1,22 @@
+public class DataProvider {
+
+    public String getSiiUrl() {
+        return "https://sii.pl";
+    }
+
+    public String getOnetUrl() {
+        return "https://www.onet.pl/";
+    }
+
+    public String getKotuszkowoUrl() {
+        return "http://kotuszkowo.pl/";
+    }
+
+    public String getFilmWebUrl() {
+        return "https://www.filmweb.pl";
+    }
+
+    public String getSeleniumUrl() {
+        return "https://www.selenium.dev/documentation/en/webdriver/";
+    }
+}
--- src/test/java/ParametrizedTest.java
+++ src/test/java/ParametrizedTest.java
@@ -6,12 +6,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ParametrizedTest extends BaseTest {
 
+    DataProvider data = new DataProvider();
+
     @ParameterizedTest
     @ValueSource(strings = {"Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska"})
     @DisplayName("Check title for Sii")
     @Tags(value={@Tag("sii"), @Tag("regression")})
     void checkTitleSii(String expectedTitle) {
-        driver.get("https://sii.pl");
+        driver.get(data.getSiiUrl());
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
     }
@@ -21,7 +23,7 @@ public class ParametrizedTest extends BaseTest {
     @DisplayName("Check title for Onet.pl")
     @Tags(value={@Tag("onet"), @Tag("regression")})
     void checkTitleOnet(String expectedTitle) {
-        driver.get("https://www.onet.pl/");
+        driver.get(data.getOnetUrl());
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
     }
@@ -31,7 +33,7 @@ public class ParametrizedTest extends BaseTest {
     @DisplayName("Check title for Kotuszkowo")
     @Tags(value={@Tag("kotuszkowo"), @Tag("regression")})
     void checkTitleKotuszkowo(String expectedTitle) {
-        driver.get("http://kotuszkowo.pl/");
+        driver.get(data.getKotuszkowoUrl());
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
     }
@@ -41,7 +43,7 @@ public class ParametrizedTest extends BaseTest {
     @DisplayName("Check title for FilmWeb")
     @Tags(value={@Tag("film"), @Tag("regression")})
     void checkTitleFilmWeb(String expectedTitle) {
-        driver.get("https://www.filmweb.pl");
+        driver.get(data.getFilmWebUrl());
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
     }
@@ -51,7 +53,7 @@ public class ParametrizedTest extends BaseTest {
     @DisplayName("Check title for Selenium")
     @Tags(value={@Tag("selenium"), @Tag("regression")})
     void checkTitleSelenium(String expectedTitle) {
-        driver.get("https://www.selenium.dev/documentation/en/webdriver/");
+        driver.get(data.getSeleniumUrl());
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
     }
