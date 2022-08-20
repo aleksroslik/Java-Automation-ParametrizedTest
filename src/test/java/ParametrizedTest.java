05871bc5efba896becc29ad5ce3cfda009466466
JavaClass <aleksroslik@gmail.com>
2022-08-20 11:41:23 +0200

    task finished

--- src/test/java/ParametrizedTest.java
+++ src/test/java/ParametrizedTest.java
@@ -9,8 +9,7 @@ public class ParametrizedTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska"})
     @DisplayName("Check title for Sii")
-    @Tag("sii")
-    @Tag("regression")
+    @Tags(value={@Tag("sii"), @Tag("regression")})
     void checkTitleSii(String expectedTitle) {
         driver.get("https://sii.pl");
         String actualTitle = driver.getTitle();
@@ -20,8 +19,7 @@ public class ParametrizedTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"Onet – Jesteś na bieżąco"})
     @DisplayName("Check title for Onet.pl")
-    @Tag("onet")
-    @Tag("regression")
+    @Tags(value={@Tag("onet"), @Tag("regression")})
     void checkTitleOnet(String expectedTitle) {
         driver.get("https://www.onet.pl/");
         String actualTitle = driver.getTitle();
@@ -31,8 +29,7 @@ public class ParametrizedTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"Kotuszkowo- blog o kotach"})
     @DisplayName("Check title for Kotuszkowo")
-    @Tag("kotuszkowo")
-    @Tag("regression")
+    @Tags(value={@Tag("kotuszkowo"), @Tag("regression")})
     void checkTitleKotuszkowo(String expectedTitle) {
         driver.get("http://kotuszkowo.pl/");
         String actualTitle = driver.getTitle();
@@ -42,8 +39,7 @@ public class ParametrizedTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"Filmweb - filmy takie jak Ty!"})
     @DisplayName("Check title for FilmWeb")
-    @Tag("film")
-    @Tag("regression")
+    @Tags(value={@Tag("film"), @Tag("regression")})
     void checkTitleFilmWeb(String expectedTitle) {
         driver.get("https://www.filmweb.pl");
         String actualTitle = driver.getTitle();
@@ -53,8 +49,7 @@ public class ParametrizedTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"WebDriver | Selenium"})
     @DisplayName("Check title for Selenium")
-    @Tag("selenium")
-    @Tag("regression")
+    @Tags(value={@Tag("selenium"), @Tag("regression")})
     void checkTitleSelenium(String expectedTitle) {
         driver.get("https://www.selenium.dev/documentation/en/webdriver/");
         String actualTitle = driver.getTitle();
