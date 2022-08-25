0c9c4ae74bc5b5e11c8f10c1421f7a8b9ceb71a6
JavaClass <aleksroslik@gmail.com>
2022-08-25 11:36:52 +0200

    logger config added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BaseTest {
+
+    public static Logger logger = LoggerFactory.getLogger(BaseTest.class);
 
     WebDriver driver;
 
@@ -16,12 +20,14 @@ public class BaseTest {
 
     @BeforeEach
     void setUp() {
+        logger.info("setup");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
     @AfterEach
     void close() {
+        logger.info("quit");
         driver.quit();
     }
 }
--- src/test/java/ParametrizedTest.java
+++ src/test/java/ParametrizedTest.java
@@ -1,16 +1,21 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ParametrizedTest extends BaseTest {
 
+    public static Logger logger = LoggerFactory.getLogger(ParametrizedTest.class);
+
     @ParameterizedTest
     @ValueSource(strings = {"Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska"})
-    @DisplayName("Check title for Sii")
+    @DisplayName("Test - Check title for Sii")
     @Tags(value={@Tag("sii"), @Tag("regression")})
     void checkTitleSii(String expectedTitle) {
+        logger.info("Checking title for Sii");
         driver.get("https://sii.pl");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
@@ -18,9 +23,10 @@ public class ParametrizedTest extends BaseTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"Onet – Jesteś na bieżąco"})
-    @DisplayName("Check title for Onet.pl")
+    @DisplayName("Test - Check title for Onet.pl")
     @Tags(value={@Tag("onet"), @Tag("regression")})
     void checkTitleOnet(String expectedTitle) {
+        logger.info("Checking title for Onet");
         driver.get("https://www.onet.pl/");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
@@ -28,9 +34,10 @@ public class ParametrizedTest extends BaseTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"Kotuszkowo- blog o kotach"})
-    @DisplayName("Check title for Kotuszkowo")
+    @DisplayName("Test - Check title for Kotuszkowo")
     @Tags(value={@Tag("kotuszkowo"), @Tag("regression")})
     void checkTitleKotuszkowo(String expectedTitle) {
+        logger.info("Checking title for Kotuszkowo");
         driver.get("http://kotuszkowo.pl/");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
@@ -38,9 +45,10 @@ public class ParametrizedTest extends BaseTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"Filmweb - filmy takie jak Ty!"})
-    @DisplayName("Check title for FilmWeb")
+    @DisplayName("Test - Check title for FilmWeb")
     @Tags(value={@Tag("film"), @Tag("regression")})
     void checkTitleFilmWeb(String expectedTitle) {
+        logger.info("Checking title for Filmweb");
         driver.get("https://www.filmweb.pl");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
@@ -48,9 +56,10 @@ public class ParametrizedTest extends BaseTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"WebDriver | Selenium"})
-    @DisplayName("Check title for Selenium")
+    @DisplayName("Test - Check title for Selenium")
     @Tags(value={@Tag("selenium"), @Tag("regression")})
     void checkTitleSelenium(String expectedTitle) {
+        logger.info("Checking title for Selenium");
         driver.get("https://www.selenium.dev/documentation/en/webdriver/");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
