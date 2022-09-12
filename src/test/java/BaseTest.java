6140823ec5d96c8226ba459cc7eb579aaf6ff2ce
JavaClass <aleksroslik@gmail.com>
2022-09-12 13:13:31 +0200

    config files added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,33 +1,32 @@
+import configuration.BrowserEnvironment;
+import configuration.EnvironmentProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BaseTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
 
-    WebDriver driver;
+    public static WebDriver driver;
+    private static BrowserEnvironment browserEnvironment;
+    public static EnvironmentProperties environmentProperties;
 
     @BeforeAll
     static void setDriver() {
         WebDriverManager.chromedriver().setup();
+        environmentProperties = EnvironmentProperties.getInstance();
+        browserEnvironment = new BrowserEnvironment();
+        driver = browserEnvironment.getDriver();
+        logger.debug("Driver started successfully");
     }
 
-    @BeforeEach
-    void setUp() {
-        logger.info("setup");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @AfterEach
-    void close() {
-        logger.info("quit");
+    @AfterAll
+    static void tearDown() {
         driver.quit();
+        logger.debug("Driver closed");
     }
 }
--- src/test/java/ParametrizedTest.java
+++ src/test/java/ParametrizedTest.java
@@ -10,58 +10,14 @@ public class ParametrizedTest extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ParametrizedTest.class);
 
-    @ParameterizedTest
-    @ValueSource(strings = {"Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska"})
-    @DisplayName("Check title for Sii")
-    @Tags(value={@Tag("sii"), @Tag("regression")})
-    void checkTitleSii(String expectedTitle) {
-        logger.info("Test - Checking title for Sii");
-        driver.get("https://sii.pl");
+    @Test
+    @DisplayName("Check title")
+    @Tag("regression")
+    void checkTitle() {
+        logger.info(">>>> Start test >>>>>");
         String actualTitle = driver.getTitle();
-        assertThat(actualTitle).isEqualTo(expectedTitle);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"Onet – Jesteś na bieżąco"})
-    @DisplayName("Check title for Onet.pl")
-    @Tags(value={@Tag("onet"), @Tag("regression")})
-    void checkTitleOnet(String expectedTitle) {
-        logger.info("Test - Checking title for Onet");
-        driver.get("https://www.onet.pl/");
-        String actualTitle = driver.getTitle();
-        assertThat(actualTitle).isEqualTo(expectedTitle);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"Kotuszkowo- blog o kotach"})
-    @DisplayName("Check title for Kotuszkowo")
-    @Tags(value={@Tag("kotuszkowo"), @Tag("regression")})
-    void checkTitleKotuszkowo(String expectedTitle) {
-        logger.info("Test - Checking title for Kotuszkowo");
-        driver.get("http://kotuszkowo.pl/");
-        String actualTitle = driver.getTitle();
-        assertThat(actualTitle).isEqualTo(expectedTitle);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"Filmweb - filmy takie jak Ty!"})
-    @DisplayName("Check title for FilmWeb")
-    @Tags(value={@Tag("film"), @Tag("regression")})
-    void checkTitleFilmWeb(String expectedTitle) {
-        logger.info("Test - Checking title for Filmweb");
-        driver.get("https://www.filmweb.pl");
-        String actualTitle = driver.getTitle();
-        assertThat(actualTitle).isEqualTo(expectedTitle);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"WebDriver | Selenium"})
-    @DisplayName("Check title for Selenium")
-    @Tags(value={@Tag("selenium"), @Tag("regression")})
-    void checkTitleSelenium(String expectedTitle) {
-        logger.info("Test - Checking title for Selenium");
-        driver.get("https://www.selenium.dev/documentation/en/webdriver/");
-        String actualTitle = driver.getTitle();
-        assertThat(actualTitle).isEqualTo(expectedTitle);
+        logger.info("Actual title: {}", actualTitle);
+        logger.info("Expected title: {}", System.getProperty("title"));
+        assertThat(actualTitle).isEqualTo(System.getProperty("title"));
     }
 }
